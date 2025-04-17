bc5e1a1a8fc3a9914c61f85ff11ac327bcabf63a
KayBroRob <kaylynbrooke.r@gmail.com>
2025-04-16 21:16:17 -0700

    update 1 adding database

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -6,5 +6,19 @@ interface JwtPayload {
 }
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.split('')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided'});
+  }
+
+  try {
+    const decoded = jwt.verify(token, secret) as JwtPayload;
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: 'Invalid token' });
+  }
   // TODO: verify the token exists and add the user data to the request object
 };
