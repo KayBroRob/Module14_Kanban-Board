87c3325d3362107cc6ba13f117499f7f10571458
KayBroRob <kaylynbrooke.r@gmail.com>
2025-06-01 22:42:17 -0700

    updated both client and server side to meet todo lists

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import authRoutes from './auth-routes.js';
-import apiRoutes from './api/index.js';
+import userRoutes from './api/user-routes.js';
+import ticketRoutes from './api/ticket-routes.js';
 import { authenticateToken } from '../middleware/auth.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 // TODO: Add authentication to the API routes
-router.use('/api', apiRoutes);
+router.use('/user-routes', authenticateToken, userRoutes);
+router.use('/ticket-routes', authenticateToken, ticketRoutes);
 
 export default router;
